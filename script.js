4c5e4dd204dc576d3e3b2b6695eaa1a00edb5277
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-05-13 16:42:38 +0700

    Create script to get product information

---
+++ script.js
@@ -0,0 +1,15 @@
+const product = {
+    product_id: 12,
+    product_name: 'baby_wipes',
+    quantity_in_stock: 35,
+    unit_price: 89,
+    product_description: [
+        'Sweet', '80 sheets', 'non frageance', 'green', 'ph balance'
+    ],
+};
+
+console.log(product.product_id);
+console.log(product.product_name);
+console.log(product.quantity_in_stock);
+console.log(product.unit_price);
+console.log(product.product_description);
